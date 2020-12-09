f25287bf3f089cba871aa6b0647dd4b474b22c96
efe <adambldwn110@gmail.com>
2020-12-09 16:20:48 +0300

    end

--- src/pages/SavedJobs.js
+++ src/pages/SavedJobs.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {useState} from 'react';
 import {SafeAreaView, View, Text} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+import { JobItem } from '../components'
 
 const SavedJobs = ()=>{
+    const [jobList, setJobList] = useState([])
+
+    AsyncStorage.getItem("@SAVED_JOBS")
+    .then(res => {
+        const list = JSON.parse(res);
+        setJobList(list);
+    })
+
     return(
         <SafeAreaView>
             <View>
-                <Text>SavedJobs</Text>
+                <FlatList
+                    data={jobList}
+                    renderItem={({item}) => <JobItem job={item}/>}
+                />
             </View>
         </SafeAreaView>
     )

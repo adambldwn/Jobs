944e9a6769e41aff0dead64180626eb30d3549ef
efe <adambldwn110@gmail.com>
2020-12-09 13:21:43 +0300

    sayfalar arası geçiş yapıldı ve veriler çekildi

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -1,72 +1,85 @@
 import React from 'react';
-import {SafeAreaView, View, Text, ScrollView} from 'react-native';
+import { SafeAreaView, View, Text, ScrollView } from 'react-native';
 
-import {TopicItem} from '../components';
+import { TopicItem } from '../components';
+import { introduction } from '../styles';
 
 const topics = [
-    {
-      id: 0,
-      name: 'Java',
-      color: 'fb5607'
-    },
-    {
-      id: 1,
-      name: 'Python',
-      color: '007f5f'
-    },
-    {
-      id: 2,
-      name: 'Javascript',
-      color: 'ffb703'
-    },
-    {
-      id: 3,
-      name: '.NET',
-      color: '023e7d'
-    },
-    {
-      id: 4,
-      name: 'Dart',
-      color: '001845'
-    },
-    {
-      id: 5,
-      name: 'Go',
-      color: 'f8961e'
-    },
-    {
-      id: 6,
-      name: 'Ruby',
-      color: 'e63946'
-    },
-    {
-      id: 7,
-      name: 'C',
-      color: 'fb8b24'
-    },
-    {
-      id: 8,
-      name: 'C++',
-      color: '06d6a0'
-    },
-  
-  ]
+  {
+    id: 0,
+    name: 'Java',
+    color: 'fb5607'
+  },
+  {
+    id: 1,
+    name: 'Python',
+    color: '007f5f'
+  },
+  {
+    id: 2,
+    name: 'Javascript',
+    color: 'ffb703'
+  },
+  {
+    id: 3,
+    name: '.NET',
+    color: '023e7d'
+  },
+  {
+    id: 4,
+    name: 'Dart',
+    color: '001845'
+  },
+  {
+    id: 5,
+    name: 'Go',
+    color: 'f8961e'
+  },
+  {
+    id: 6,
+    name: 'Ruby',
+    color: 'e63946'
+  },
+  {
+    id: 7,
+    name: 'C',
+    color: 'fb8b24'
+  },
+  {
+    id: 8,
+    name: 'C++',
+    color: '06d6a0'
+  },
 
-const Introduction = ()=>{
-    return(
-        <SafeAreaView style={{flex: 1}}>
-            <View style={{flex: 1}}>
-                <Text>Introduction</Text>
-                <ScrollView>
-                  {
-                    topics.map((t)=>{
-                      return <TopicItem key={t.id} item={t} />;
-                    })
-                  }
-                </ScrollView>
-            </View>
-        </SafeAreaView>
-    )
+]
+
+const Introduction = (props) => {
+
+  function selectLanguage(lang){
+    props.navigation.navigate("Jobs", { selectedLanguage: lang });
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+
+      <View style={{ flex: 1 }}>
+
+        <View style={introduction.banner}>
+          <Text style={introduction.bannerText}>Aradığınız dili seçiniz</Text>
+        </View>
+
+        <ScrollView horizontal contentContainerStyle={{ alignItems: 'center' }}>
+          {
+            topics.map((t) => {
+              return <TopicItem key={t.id} item={t} onSelect={() => selectLanguage(t.name)} />;
+            })
+          }
+        </ScrollView>
+
+      </View>
+
+    </SafeAreaView>
+  )
 }
 
-export {Introduction};
+export { Introduction };
--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import {SafeAreaView, View, Text} from 'react-native';
 
-const Jobs = ()=>{
+const Jobs = (props)=>{
+    const {selectedLanguage} = props.route.params;
+
+    const fetchData = async ()=>{
+        const response = await axios.get(`https://jobs.github.com/positions.json?search=${selectedLanguage.toLowerCase()}`);
+    }
+
+    useEffect(()=>{
+        fetchData()
+    },[])
     return(
         <SafeAreaView>
             <View>
                 <Text>Jobs</Text>
+                <Text>{selectedLanguage}</Text>
             </View>
         </SafeAreaView>
     )
